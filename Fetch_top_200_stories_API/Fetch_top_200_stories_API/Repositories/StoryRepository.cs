dc10be2a78cb2d44f7d4fa7aa7564547c198a7ca
Raju7.Gupta <raju7.gupta@ril.com>
2024-07-19 17:59:19 +0530

    added swagger and create  get list api

---
+++ Fetch_top_200_stories_API/Fetch_top_200_stories_API/Repositories/StoryRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration.UserSecrets;
+using System.Data;
+
+namespace Fetch_top_200_stories_API.Repositories
+{
+    public class StoryRepository
+    {
+        public async Task<string> GetStoryList()
+        {
+            var content = "";
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var url = "https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty";
+                    var response = await client.GetAsync(url);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        content = await response.Content.ReadAsStringAsync();
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+
+            }
+
+            return content;
+        }
+    }
+}
---
+++ Fetch_top_200_stories_API/Fetch_top_200_stories_API/Services/StoryService.cs
@@ -0,0 +1,41 @@
+﻿using Fetch_top_200_stories_API.Models;
+using Fetch_top_200_stories_API.Repositories;
+using System.Data;
+
+namespace Fetch_top_200_stories_API.Services
+{
+    public class StoryService
+    {
+        StoryRepository repo = new StoryRepository();
+        public async Task<Result> GetStoryList()
+        {
+            Result res = new Result();
+
+            try
+            {
+                string str = await repo.GetStoryList(); // Make asynchronous call
+                if (!string.IsNullOrEmpty(str))
+                {
+                    res.Data = str;
+                    res.Success = true;
+                    res.StatusCode = 200;
+                    res.UserMsg = "Data retrieved successfully!!";
+                }
+                else
+                {
+                    res.Success = false;
+                    res.UserMsg = "Data not retrieved.";
+                    res.StatusCode = 400;
+                }
+                return res;
+            }
+            catch (Exception ex)
+            {
+                res.Success = false;
+                res.UserMsg = ex.Message;
+                res.StatusCode = 500;
+                return res;
+            }
+        }
+    }
+}
